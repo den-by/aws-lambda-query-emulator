a5218c3cfce01743c99341033776c1a239ee40cc
Denis <denis.da.by@gmail.com>
2020-07-12 22:21:52 +0300

    1

--- index.js
+++ index.js
@@ -15,3 +15,31 @@ module.exports.addResolversToSchema = require('@graphql-tools/schema').addResolv
 // module.exports.ApolloServer = ApolloServer;
 // module.exports.express = express;
 // module.exports.expressApollo = expressApollo;
+
+
+
+// const sdfsd = __dirname;
+
+
+module.exports.contextAdapterCreate = function () {
+    const fs = require('fs');
+    const argv = require('minimist')(process.argv.slice(2));
+    const  rootDir = process.cwd();
+    const contextPatch = argv['userType'] === 'purchaser' ? 'tests/test_create_purchaser.json' : 'tests/test_create_user.json';
+    const contextJsonString = fs.readFileSync(rootDir+'/'+contextPatch);
+
+    require('dotenv').config({path: rootDir+'/config/env/.env.test'});
+    const context = JSON.parse(String(contextJsonString));
+    const {availableParameters} = require(rootDir+'/helper/context/context.js');
+    const contextAdapter = Object.keys(context).reduce(function (previous, key) {
+        if (availableParameters[key]) {
+            let newKey = availableParameters[key];
+            previous[newKey] = context[key];
+        } else {
+            previous[key] = context[key];
+        }
+
+        return previous;
+    }, {});
+    return contextAdapter
+}
