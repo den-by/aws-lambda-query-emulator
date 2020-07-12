5a0a9fef550b4bbe7741d028480fe0118486fe6a
Denis <denis.da.by@gmail.com>
2020-07-12 20:52:55 +0300

    1

--- index.js
+++ index.js
@@ -8,9 +8,10 @@
 // const {ApolloServer, express,expressApollo } = require('./server');
 
 module.exports.server = require('./server');
-module.exports.loadSchemaSync = require('@graphql-tools/load');
-module.exports.GraphQLFileLoader = require('@graphql-tools/graphql-file-loader');
-module.exports.addResolversToSchema = require('@graphql-tools/schema');
+
+module.exports.loadSchemaSync = require('@graphql-tools/load').loadSchemaSync;
+module.exports.GraphQLFileLoader = require('@graphql-tools/graphql-file-loader').GraphQLFileLoader;
+module.exports.addResolversToSchema = require('@graphql-tools/schema').addResolversToSchema;
 // module.exports.ApolloServer = ApolloServer;
 // module.exports.express = express;
 // module.exports.expressApollo = expressApollo;
