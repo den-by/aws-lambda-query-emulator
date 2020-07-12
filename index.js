e932f2227cc84cae107a0b0e2dbf61cfd71527a8
Denis <denis.da.by@gmail.com>
2020-07-12 22:41:13 +0300

    1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// const {loadSchemaSync} =
-// const {GraphQLFileLoader} =
-// const {addResolversToSchema} =
+const {loadSchemaSync} = require('@graphql-tools/load');
+const {GraphQLFileLoader} = require('@graphql-tools/graphql-file-loader');
+const {addResolversToSchema} = require('@graphql-tools/schema');
 // const {ApolloServer, express,expressApollo } = require ('apollo-server-express');
 // module.exports.graphql = require('graphql')
 // const ApolloServer = require('apollo-server').ApolloServer
@@ -42,4 +42,52 @@ module.exports.contextAdapterCreate = function () {
         return previous;
     }, {});
     return contextAdapter
+}
+
+
+module.exports.addResolversToSchemas = function (contextAdapter){
+    const  rootDir = process.cwd()
+    const fs = require('fs');
+    const serverlessString = fs.readFileSync(rootDir+'/serverless.yml', 'utf8');
+    const serverless = require('yaml').parse(serverlessString);
+    const mapping = serverless.custom['appSync']['mappingTemplates'];
+
+    const path = require('path');
+    const normalizedPath = path.join(rootDir, 'resolvers');
+    let resolversFunctions = {};
+    fs.readdirSync(normalizedPath).forEach(function (dir) {
+        fs.readdirSync(path.join(normalizedPath, dir)).forEach(function (file) {
+            const patch = normalizedPath+'/' + dir + '/' + file;
+            try {
+                resolversFunctions = {...resolversFunctions, ...require(patch)};
+            } catch (e) {
+                console.log('file not load: ' + patch);
+            }
+        });
+    });
+
+    const resolvers = mapping.reduce(function (sum, current) {
+        const type = current.type;
+        const field = current.field;
+        let request;
+        if (type === 'Query' || type === 'Mutation') {
+            request = (_, atr) => resolversFunctions[field]({...atr, ...contextAdapter});
+            sum[type] = {...sum[type || {}], [field]: request};
+        }
+        return sum;
+    }, {});
+
+
+    const schema = loadSchemaSync([rootDir+'/aws.graphql', rootDir+'/graphql/*.graphql'], {
+        loaders: [
+            new GraphQLFileLoader(),
+        ]
+    });
+
+    const schemaWithResolvers = addResolversToSchema({
+        schema,
+        resolvers,
+    });
+    return schemaWithResolvers;
+
 }
