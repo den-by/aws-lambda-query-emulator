0d80196f0c5da6c23827af676d6e4dee3d81aa98
Denis <denis.da.by@gmail.com>
2020-07-12 20:47:18 +0300

    1

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const {loadSchemaSync} = require('@graphql-tools/load');
-const {GraphQLFileLoader} = require('@graphql-tools/graphql-file-loader');
-const {addResolversToSchema} = require('@graphql-tools/schema');
+// const {loadSchemaSync} =
+// const {GraphQLFileLoader} =
+// const {addResolversToSchema} =
 // const {ApolloServer, express,expressApollo } = require ('apollo-server-express');
 // module.exports.graphql = require('graphql')
 // const ApolloServer = require('apollo-server').ApolloServer
 
-const {ApolloServer, express,expressApollo } = require('./server');
+// const {ApolloServer, express,expressApollo } = require('./server');
 
-// const server = require('./server');
-// module.exports.toSecond = value => value*2
-// module.exports.toThird = value => value*3
-
-module.exports.loadSchemaSync = loadSchemaSync;
-module.exports.GraphQLFileLoader = GraphQLFileLoader;
-module.exports.addResolversToSchema = addResolversToSchema;
-module.exports.ApolloServer = ApolloServer;
-module.exports.express = express;
-module.exports.expressApollo = expressApollo;
+module.exports.server = require('./server');
+module.exports.loadSchemaSync = require('@graphql-tools/load');
+module.exports.GraphQLFileLoader = require('@graphql-tools/graphql-file-loader');
+module.exports.addResolversToSchema = require('@graphql-tools/schema');
+// module.exports.ApolloServer = ApolloServer;
+// module.exports.express = express;
+// module.exports.expressApollo = expressApollo;
