d5242bd70d9f442e235a7bc784c349a8477d879b
denis <denis.da.by@gmail.com>
2021-05-03 15:25:27 +0300

    make simple

--- index.js
+++ index.js
@@ -2,28 +2,16 @@ const {loadSchemaSync} = require('@graphql-tools/load');
 const {GraphQLFileLoader} = require('@graphql-tools/graphql-file-loader');
 const {addResolversToSchema} = require('@graphql-tools/schema');
 const server = require('./server');
-const fs = require('fs');
 const argv = require('minimist')(process.argv.slice(2));
 const rootDir = argv['rootDir'];
 require('dotenv').config({path: rootDir + '/config/env/.env.test'});
-const getContext = function () {
-    const contextPatch = argv['userType'] === 'purchaser' ? 'tests/test_create_purchaser.json' : 'tests/test_create_user.json';
-    const contextJsonString = fs.readFileSync(rootDir + '/' + contextPatch);
-    const context = JSON.parse(String(contextJsonString));
-    const snakeCase = require('lodash.snakecase');
 
-    return Object.entries(context).reduce((total, item) => {
-        total[snakeCase(item[0])] = item[1];
-        return total
-    }, {})
-};
-
-const addResolversToSchemas = function (context) {
+const addResolversToSchemas = function () {
 
     const path = require('path');
 
-    const normalizedPath = path.join(rootDir, 'function/graphql.js');
-    // const normalizedPath = './dist/main.js';
+    const normalizedPath =  argv['start_bundle'] ? './dist/main.js' : path.join(rootDir, 'function/graphql.js');
+
     const exports = require(normalizedPath)
 
     const schema = loadSchemaSync([process.cwd() + '/aws.graphql', rootDir + '/graphql/*.graphql'], {
@@ -35,9 +23,10 @@ const addResolversToSchemas = function (context) {
     const event = {
         arguments: {},
         security: {
+           sourceIp: '0.0.0.0',
             claims: {
-                ['custom:account_id']: context.account_id,
-                ['custom:is_purchaser']: context.is_purchaser
+                ['custom:account_id']: argv.account_id,
+                ['custom:is_purchaser']: argv.is_purchaser
             }
         }
     }
@@ -89,7 +78,6 @@ const addResolversToSchemas = function (context) {
 };
 
 module.exports.startServer = function () {
-    const context = getContext()
-    const schemaWithResolvers = addResolversToSchemas(context);
+    const schemaWithResolvers = addResolversToSchemas();
     server.expressApollo(schemaWithResolvers);
 }
